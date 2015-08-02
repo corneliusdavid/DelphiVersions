451dc3dc881e4ba0e610e4bf4d336f39c6e93715
David Cornelius <dev@corneliusconcepts.com>
2015-08-02 10:17:42 -0700

    added many more versions, up to XE8; included platform definitions

--- ufrmDelphiVersions.dfm
+++ ufrmDelphiVersions.dfm
@@ -1,9 +1,9 @@
 object frmDelphiVersions: TfrmDelphiVersions
   Left = 263
   Top = 235
+  Width = 560
+  Height = 426
   Caption = 'Delphi Versions'
-  ClientHeight = 340
-  ClientWidth = 438
   Color = clBtnFace
   Constraints.MinWidth = 450
   Font.Charset = DEFAULT_CHARSET
@@ -13,15 +13,12 @@ object frmDelphiVersions: TfrmDelphiVersions
   Font.Style = []
   OldCreateOrder = False
   OnActivate = FormActivate
-  DesignSize = (
-    438
-    340)
   PixelsPerInch = 96
   TextHeight = 16
   object Label1: TLabel
     Left = 8
     Top = 8
-    Width = 422
+    Width = 511
     Height = 97
     Anchors = [akLeft, akTop, akRight]
     AutoSize = False
@@ -33,12 +30,73 @@ object frmDelphiVersions: TfrmDelphiVersions
       'ned for that version.'
     WordWrap = True
   end
+  object Label2: TLabel
+    Left = 8
+    Top = 341
+    Width = 54
+    Height = 13
+    Caption = 'Reference:'
+    Font.Charset = DEFAULT_CHARSET
+    Font.Color = clWindowText
+    Font.Height = -11
+    Font.Name = 'Tahoma'
+    Font.Style = []
+    ParentFont = False
+  end
+  object Label3: TLabel
+    Left = 8
+    Top = 363
+    Width = 54
+    Height = 13
+    Caption = 'Reference:'
+    Font.Charset = DEFAULT_CHARSET
+    Font.Color = clWindowText
+    Font.Height = -11
+    Font.Name = 'Tahoma'
+    Font.Style = []
+    ParentFont = False
+  end
   object lbDefs: TListBox
     Left = 8
-    Top = 108
-    Width = 422
+    Top = 111
+    Width = 511
     Height = 224
-    Anchors = [akLeft, akTop, akRight, akBottom]
+    Anchors = [akLeft, akTop, akRight]
+    ItemHeight = 16
     TabOrder = 0
+  end
+  object Edit1: TEdit
+    Left = 68
+    Top = 338
+    Width = 451
+    Height = 22
+    Font.Charset = DEFAULT_CHARSET
+    Font.Color = clWindowText
+    Font.Height = -11
+    Font.Name = 'Arial'
+    Font.Style = []
+    ParentFont = False
+    ReadOnly = True
+    TabOrder = 1
+    Text = 
+      'http://docwiki.embarcadero.com/RADStudio/XE8/en/Compiler_Version' +
+      's'
+  end
+  object Edit2: TEdit
+    Left = 68
+    Top = 360
+    Width = 451
+    Height = 22
+    Font.Charset = DEFAULT_CHARSET
+    Font.Color = clWindowText
+    Font.Height = -11
+    Font.Name = 'Arial'
+    Font.Style = []
+    ParentFont = False
+    ReadOnly = True
+    TabOrder = 2
+    Text = 
+      'http://docwiki.embarcadero.com/RADStudio/XE8/en/Conditional_comp' +
+      'ilation_%28Delphi%29'
   end
 end
--- ufrmDelphiVersions.pas
+++ ufrmDelphiVersions.pas
@@ -10,6 +10,10 @@ type
   TfrmDelphiVersions = class(TForm)
     Label1: TLabel;
     lbDefs: TListBox;
+    Label2: TLabel;
+    Edit1: TEdit;
+    Label3: TLabel;
+    Edit2: TEdit;
     procedure FormActivate(Sender: TObject);
   end;
 
@@ -22,27 +26,62 @@ implementation
 
 procedure TfrmDelphiVersions.FormActivate(Sender: TObject);
 begin
+  // platform
+  {$IFDEF LINUX}      lbDefs.Items.Add('LINUX');                {$ENDIF}
+  {$IFDEF LINUX32}    lbDefs.Items.Add('LINUX32');              {$ENDIF}
+  {$IFDEF MSWINDOWS}  lbDefs.Items.Add('MSWINDOWS');            {$ENDIF}
+  {$IFDEF WIN32}      lbDefs.Items.Add('WIN32');                {$ENDIF}
+  {$IFDEF WIN64}      lbDefs.Items.Add('WIN64');                {$ENDIF}
+  {$IFDEF IOS}        lbDefs.Items.Add('IOS');                  {$ENDIF}
+  {$IFDEF NATIVECODE} lbDefs.Items.Add('NATIVECODE');           {$ENDIF}
+  {$IFDEF MACOS}      lbDefs.Items.Add('MACOS');                {$ENDIF}
+  {$IFDEF MACOS32}    lbDefs.Items.Add('MACOS32');              {$ENDIF}
+  {$IFDEF POSIX}      lbDefs.Items.Add('POSIX');                {$ENDIF}
+  {$IFDEF POSIX32}    lbDefs.Items.Add('POSIX32');              {$ENDIF}
+  {$IFDEF ANDROID}    lbDefs.Items.Add('ANDROID');              {$ENDIF}
+
+  // cpu
+  {$IFDEF CPU386}     lbDefs.Items.Add('CPU386');               {$ENDIF}
+  {$IFDEF CPUARM}     lbDefs.Items.Add('CPUARM');               {$ENDIF}
+  {$IFDEF CPUARM64}   lbDefs.Items.Add('CPUARM64');             {$ENDIF}
+  {$IFDEF CPUX86}     lbDefs.Items.Add('CPUX86');               {$ENDIF}
+  {$IFDEF CPUX64}     lbDefs.Items.Add('CPUX64');               {$ENDIF}
+  {$IFDEF CPU32BITS}  lbDefs.Items.Add('CPU32BITS');            {$ENDIF}
+  {$IFDEF CPU64BITS}  lbDefs.Items.Add('CPU64BITS');            {$ENDIF}
+
+  // application type
+  {$IFDEF CONSOLE}    lbDefs.Items.Add('CONSOLE');              {$ENDIF}
+
+  // default build configurations
+  {$IFDEF RELEASE}    lbDefs.Items.Add('RELEASE');              {$ENDIF}
+  {$IFDEF DEBUG}      lbDefs.Items.Add('DEBUG');                {$ENDIF}
+
+  // other options
+  {$IFDEF CONDITIONALEXPRESSIONS} lbDefs.Items.Add('CONDITIONALEXPRESSIONS'); {$ENDIF}
+
+  // delphi version
   {$IFDEF VER80}      lbDefs.Items.Add('VER80 (Delphi 1)');     {$ENDIF}
   {$IFDEF VER90}      lbDefs.Items.Add('VER90 (Delphi 2)');     {$ENDIF}
   {$IFDEF VER100}     lbDefs.Items.Add('VER100 (Delphi 3)');    {$ENDIF}
   {$IFDEF VER120}     lbDefs.Items.Add('VER120 (Delphi 4)');    {$ENDIF}
   {$IFDEF VER130}     lbDefs.Items.Add('VER130 (Delphi 5)');    {$ENDIF}
-  {$IFDEF VER140}     lbDefs.Items.Add('VER140 (Delphi 6)');    {$ENDIF}
-  {$IFDEF VER150}     lbDefs.Items.Add('VER150 (Delphi 7)');    {$ENDIF}
-  {$IFDEF VER160}     lbDefs.Items.Add('VER160 (Delphi 8)');    {$ENDIF}
-  {$IFDEF VER170}     lbDefs.Items.Add('VER170 (Delphi 2005)'); {$ENDIF}
-  {$IFDEF VER180}     lbDefs.Items.Add('VER180 (Delphi 2006 & 2007)'); {$ENDIF}
-  {$IFDEF VER185}     lbDefs.Items.Add('VER185 (Delphi 2007)'); {$ENDIF}
-  {$IFDEF VER200}     lbDefs.Items.Add('VER200 (Delphi 2009)'); {$ENDIF}
-  {$IFDEF VER210}     lbDefs.Items.Add('VER210 (Delphi 2010)'); {$ENDIF}
-  {$IFDEF VER220}     lbDefs.Items.Add('VER220 (Delphi ?)'); {$ENDIF}
-  {$IFDEF VER230}     lbDefs.Items.Add('VER230 (Delphi ?)'); {$ENDIF}
-  {$IFDEF LINUX}      lbDefs.Items.Add('LINUX');                {$ENDIF}
-  {$IFDEF WIN32}      lbDefs.Items.Add('WIN32');                {$ENDIF}
-  {$IFDEF MSWINDOWS}  lbDefs.Items.Add('MSWINDOWS');            {$ENDIF}
-  {$IFDEF CONSOLE}    lbDefs.Items.Add('CONSOLE');              {$ENDIF}
-  {$IFDEF RELEASE}    lbDefs.Items.Add('RELEASE');              {$ENDIF}
-  {$IFDEF DEBUG}      lbDefs.Items.Add('DEBUG');                {$ENDIF}
+  {$IFDEF VER140}     lbDefs.Items.Add('VER140 (Delphi 6, package version 60)');    {$ENDIF}
+  {$IFDEF VER150}     lbDefs.Items.Add('VER150 (Delphi 7, package version 70)');    {$ENDIF}
+  {$IFDEF VER160}     lbDefs.Items.Add('VER160 (Delphi 8, package version 80)');    {$ENDIF}
+  {$IFDEF VER170}     lbDefs.Items.Add('VER170 (Delphi 2005, package version 90)'); {$ENDIF}
+  {$IFDEF VER180}     lbDefs.Items.Add('VER180 (Delphi 2006, package version 100)'); {$ENDIF}
+  {$IFDEF VER180}     lbDefs.Items.Add('VER180 (Delphi 2007, package version 100)'); {$ENDIF}
+  {$IFDEF VER185}     lbDefs.Items.Add('VER185 (Delphi 2007, package version 110)'); {$ENDIF}
+  {$IFDEF VER200}     lbDefs.Items.Add('VER200 (Delphi 2009, package version 120)'); {$ENDIF}
+  {$IFDEF VER210}     lbDefs.Items.Add('VER210 (Delphi 2010, package version 140)'); {$ENDIF}
+  {$IFDEF VER220}     lbDefs.Items.Add('VER220 (Delphi XE, package version 150)');   {$ENDIF}
+  {$IFDEF VER230}     lbDefs.Items.Add('VER230 (Delphi XE2, package version 160, 161)');  {$ENDIF}
+  {$IFDEF VER240}     lbDefs.Items.Add('VER240 (Delphi XE3, package version 170)');  {$ENDIF}
+  {$IFDEF VER250}     lbDefs.Items.Add('VER250 (Delphi XE4, package version 180)');  {$ENDIF}
+  {$IFDEF VER260}     lbDefs.Items.Add('VER260 (Delphi XE5, package version 190)');  {$ENDIF}
+  {$IFDEF VER270}     lbDefs.Items.Add('VER270 (Delphi XE6, package version 200)');  {$ENDIF}
+  {$IFDEF VER280}     lbDefs.Items.Add('VER280 (Delphi XE7, package version 210)');  {$ENDIF}
+  {$IFDEF VER290}     lbDefs.Items.Add('VER290 (Delphi XE8, package version 220)');  {$ENDIF}
 end;
 
 end.
