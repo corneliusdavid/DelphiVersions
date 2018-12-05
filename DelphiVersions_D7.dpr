2810a24f576625ee83074570fcc85fd0066a3a89
David Cornelius <dev@corneliusconcepts.com>
2018-12-05 15:36:24 -0800

    added support for Delphi 7, Delphi 2007, and Delphi 10.3 Rio

---
+++ DelphiVersions_D7.dpr
@@ -0,0 +1,15 @@
+program DelphiVersions;
+
+uses
+  Forms,
+  ufrmDelphiVersions in 'ufrmDelphiVersions.pas' {frmDelphiVersions},
+  uConditionalList in 'uConditionalList.pas';
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.Title := 'Delphi Versions';
+  Application.CreateForm(TfrmDelphiVersions, frmDelphiVersions);
+  Application.Run;
+end.
