2733bb96721f7d75a8fc826d3ec2cfed7564f86e
David Cornelius <dev@corneliusconcepts.com>
2021-09-13 23:11:01 -0700

    added projects and project group for Delphi 11 Alexandria

---
+++ 11Alexandria/DelphiVersionsVCL.dpr
@@ -0,0 +1,17 @@
+program DelphiVersionsVCL;
+
+uses
+  Forms,
+  uConditionalList in '..\uConditionalList.pas',
+  ufrmDelphiVersions in '..\ufrmDelphiVersions.pas' {frmDelphiVersions},
+  System.SysUtils, System.IOUtils,
+  Vcl.Themes, Vcl.Styles;
+
+begin
+  Application.Initialize;
+  TStyleManager.LoadFromFile(TPath.Combine(ExtractFilePath(Application.ExeName), 'CopperDark.vsf'));
+  TStyleManager.TrySetStyle('CopperDark');
+  Application.Title := 'Delphi Versions';
+  Application.CreateForm(TfrmDelphiVersions, frmDelphiVersions);
+  Application.Run;
+end.
