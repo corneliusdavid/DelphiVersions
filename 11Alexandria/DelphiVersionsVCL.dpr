1044d891d0b787b0d15bf551a759e27b8df07993
David Cornelius <dev@corneliusconcepts.com>
2023-09-25 08:17:35 -0700

    VCL: added a build event to copy the Glow style file locally; added code to load the copied style file locally at runtime

--- 11Alexandria/DelphiVersionsVCL.dpr
+++ 11Alexandria/DelphiVersionsVCL.dpr
@@ -1,16 +1,20 @@
 program DelphiVersionsVCL;
 
+
+
 uses
   Forms,
   uConditionalList in '..\uConditionalList.pas',
   ufrmDelphiVersions in '..\ufrmDelphiVersions.pas' {frmDelphiVersions},
-  System.SysUtils, System.IOUtils,
-  Vcl.Themes, Vcl.Styles;
+  System.SysUtils,
+  System.IOUtils,
+  Vcl.Themes,
+  Vcl.Styles;
 
 begin
   Application.Initialize;
-  TStyleManager.LoadFromFile(TPath.Combine(ExtractFilePath(Application.ExeName), 'CopperDark.vsf'));
-  TStyleManager.TrySetStyle('CopperDark');
+  TStyleManager.LoadFromFile('Glow.vsf');
+  TStyleManager.TrySetStyle('Glow');
   Application.Title := 'Delphi Versions';
   Application.CreateForm(TfrmDelphiVersions, frmDelphiVersions);
   Application.Run;
